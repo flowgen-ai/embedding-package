1978a80c1dae28d1a9bc57e4d61349dd652fcb2c
aidin <aidin.firouzabady@gmail.com>
2024-11-07 12:37:19 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 setup(
     name="flowgen_embedding",
     version="0.1.0",
+    packages=find_packages(include=["flowgen_emedding", "flowgen_emedding.utils"]),
     description="A vector embedding service with elasticsearch and langchain",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="aidin",
     author_email="aidin@cube10.io",
-    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "langchain-core",
